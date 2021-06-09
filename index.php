b3e49df6029d0ce6ee5261a036b71fc059b46ed1
VENANSIUS MARIO TANDO _ <s160419091@student.ubaya.ac.id>
2021-06-09 23:50:54 +0700

    login wes, chat wes

--- index.php
+++ index.php
@@ -20,10 +20,11 @@
 <head>
 	<title>Index</title>
 	<script type="text/javascript" src="jquery.js"></script>
-	<link rel="stylesheet" href="css/css.css">
+	<link rel="stylesheet" href="css.css">
 </head>
 <body>
-	<form method="post" action="login_process.php"  enctype="multipart/form-data">
+	<div class="wrapper">
+	<!-- <form method="post" action="login_process.php"  enctype="multipart/form-data">
 		<input type="submit" name="btnlogout" value="LOGOUT">
 	</form>
 	<label>Welcome <?php echo $name ?>!</label><br>
@@ -49,6 +50,233 @@
 		<div id="textbox">
 			<input type="textbox" id="txtmessage"><input type="button" id="btnsend" value="SEND">
 		</div>
+	</div> -->
+	<header>
+			<div class="pembungkus-kiri">
+				<div class="gambar">
+					<img src="billy.jpg" alt="">
+				</div>
+				<div class="nama">
+					<h4>Billy Renatasivu</h4>
+				</div>
+			</div>
+			<a href="#" class="pembungkus-kanan">Logout</a>
+	</header>
+	<nav>
+		<input type="text" placeholder="Search..">
+	</nav>
+	<aside>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+		<div class="list-chat">
+			<div class="profile-picture">
+				<img src="billy.jpg">
+			</div>
+			<div class="info-chat">
+				<div class="profile-name">
+					<h5>Venansius Mario</h5>
+				</div>
+				<div class="chat-content">
+					<p>Test Message</p>
+				</div>
+			</div>
+		</div>
+	</aside>
+	<main>
+		<div class="content">
+			<div class="chat-keluar">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-masuk">
+				<img src="billy.jpg" alt="">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-keluar">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-masuk">
+				<img src="billy.jpg" alt="">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-keluar">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-masuk">
+				<img src="billy.jpg" alt="">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-keluar">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+			<div class="chat-masuk">
+				<img src="billy.jpg" alt="">
+				<div class="details">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo tenetur mollitia, possimus illo dolor, consectetur libero dolores, sint iusto quasi, ut molestiae cum aliquid suscipit commodi nemo nam minus labore!</p>
+				</div>
+			</div>
+		</div>
+		
+
+		<div class="send-message">
+			<img src="">
+			<input type="text">
+		</div>
+	</main>
 	</div>
 
 	<script type="text/javascript">
